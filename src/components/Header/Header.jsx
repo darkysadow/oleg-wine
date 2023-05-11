65df8019c3fed95239a437738a2e8e9ce1cc6acc
Oleksandr Poberezhniak <oleksandr.poberezhniak@gmail.com>
2023-05-11 18:56:42 +0300

    Added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from '../../img/logo.png';
+import s from './Header.module.scss';
+
+const Header = () => {
+    return (<div className="container">
+        <nav className={s.navbar}>
+            <ul className={s.left}>
+                <li>Головна</li>
+                <li>Про нас</li>
+                <li>Партнери</li>
+            </ul>
+            <div className={s.logo}>
+                <img src={logo} alt="" />
+            </div>
+            <ul className={s.right}>
+                <li>Асортимент</li>
+                <li>Контакти</li>
+                <li>Кошик</li>
+            </ul>
+        </nav>
+        
+    </div>);
+}
+
+export default Header;
