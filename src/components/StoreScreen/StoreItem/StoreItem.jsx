337ce537754d246fccbfc3c0341897a69ed899eb
Oleksandr Poberezhniak <oleksandr.poberezhniak@gmail.com>
2023-05-17 11:39:44 +0300

    Remaked styles for each component for adaptive layout

--- src/components/StoreScreen/StoreItem/StoreItem.jsx
+++ src/components/StoreScreen/StoreItem/StoreItem.jsx
@@ -5,24 +5,30 @@ import grape from './../../../img/grape.png';
 import dweller from './../../../img/dweller.png';
 
 const StoreItem = (props) => {
-    const checkNominal = () => {if(!props.item.nominal) {
-        if(props.item.category === 'wine') {
-            return "грн/л"
-        } else if (props.item.category === "grape") {
-            return "грн/кг"
-        } else {
-            return "грн/шт"
-        } 
-    }}
+    const checkNominal = () => {
+        if (!props.item.nominal) {
+            if (props.item.category === 'wine') {
+                return "грн/л"
+            } else if (props.item.category === "grape") {
+                return "грн/кг"
+            } else {
+                return "грн/шт"
+            }
+        }
+    }
     return (
         <div className={s.storeItem}>
-            {props.item.category === 'wine' && <img src={wine} alt="" />}
-            {props.item.category === 'grape' && <img src={grape} alt="" />}
-            {props.item.category === 'dweller' && <img src={dweller} alt="" />}
-            <p>{props.item.title}</p>
-            <p>
-                {props.item.nominal ? props.item.price + ' ' + props.item.nominal : props.item.price + checkNominal()}
-            </p>
+            <div className={s.storeItemImage}>
+                {props.item.category === 'wine' && <img src={wine} alt="" />}
+                {props.item.category === 'grape' && <img src={grape} alt="" />}
+                {props.item.category === 'dweller' && <img src={dweller} alt="" />}
+            </div>
+            <div className={s.storeItemInfo}>
+                <p>{props.item.title}</p>
+                <p>
+                    {props.item.nominal ? props.item.price + ' ' + props.item.nominal : props.item.price + checkNominal()}
+                </p>
+            </div>
         </div>
     );
 }
