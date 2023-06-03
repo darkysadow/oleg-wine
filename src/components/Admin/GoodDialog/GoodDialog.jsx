b551772ed24c35c0aa5e582e8e715ead202952f3
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-06-03 19:39:07 +0300

    Refactored GoodDialog, added validation to form, and enabling of submit button. Changed styles for Admin component and AdminItem

--- src/components/Admin/GoodDialog/GoodDialog.jsx
+++ src/components/Admin/GoodDialog/GoodDialog.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import s from './GoodDialog.module.scss';
-import { Avatar, Button, Dialog, DialogActions, DialogContent, FormControlLabel, MenuItem, Radio, RadioGroup, Select, Stack, TextField, Typography, createTheme } from "@mui/material";
+import { Avatar, Button, Dialog, DialogActions, DialogContent, FormControlLabel, MenuItem, Radio, RadioGroup, Select, Stack, TextField, ThemeProvider, Typography, createTheme } from "@mui/material";
 import { useEffect } from "react";
-import { yellow } from "@mui/material/colors";
+import { purple, yellow } from "@mui/material/colors";
 
 
 const GoodDialog = (props) => {
@@ -19,7 +19,7 @@ const GoodDialog = (props) => {
             imgBucketURL: "",
             imgURL: "",
             price: undefined,
-          }))
+        }))
     }, [props.edit, props.showDialog])
 
     const updateFormField = (event, field) => {
@@ -48,88 +48,92 @@ const GoodDialog = (props) => {
             imgBucketURL: "",
             imgURL: "",
             price: undefined,
-          });
+        });
     }
 
     const isSubmitting = () => {
         console.log('isSubmitting');
     }
     const isDisabled = () => {
-        return true;
+        console.log(props.formFields.goodName.length === 0 || props.formFields.description.length === 0 ||
+            props.formFields.category === 'Оберіть категорію' || props.formFields.fileName === "Файл не обрано!");
+            return (props.formFields.goodName.length === 0 || props.formFields.description.length === 0 ||
+            props.formFields.category === 'Оберіть категорію' || props.formFields.fileName === "Файл не обрано!" ||
+            !props.formFields.available || !props.formFields.price || props.formFields.price <= 0)
     }
 
     const theme = createTheme({
         palette: {
-          primary: {
-              // Purple and green play nicely together.
-              main: yellow[800],
-          },
-          secondary: {
-              // This is green.A700 as hex.
-              main: '#6d1a1bcf',
-          },
-          textColor: {
-              main: '#000'
-          }
-      },
-      })
+            primary: {
+                main: purple[800],
+            },
+            secondary: {
+                main: purple[300],
+            },
+            textColor: {
+                main: '#000'
+            }
+        },
+    })
 
     return (
         props.formFields &&
-        <Dialog
-            classes={{paper: s.dialog}}
-            onClose={closeDialog}
-            open={props.showDialog}
-            component="form">
-            <Typography variant="h4" className={s.dialogTitle}>
-                {isEdit ? "РЕДАГУВАТИ" : "ДОДАТИ"} ТОВАР
-            </Typography>
-            <DialogContent className={s.dialogFields} >
-                <Stack direction="row" spacing={2} className={s.dialogGoodImage}>
-                    {(isEdit) && <Avatar alt="receipt image" src={props.formFields.imgURL} />}
-                    <Button variant="outlined" component="label" color="secondary" className={s.dialogImgPicker}>
-                        Обрати зображення
-                        <input type="file" hidden onInput={(event) => { setFileData(event.target) }} accept=".jpg, .jpeg, .png, .jfif" />
-                    </Button>
-                    <Typography className={s.dialogPickedImg}>{props.formFields.fileName}</Typography>
-                </Stack>
-                <TextField size="small" id="filled-basic" label="Назва страви" variant="filled" value={!props.formFields && props.formFields.goodName.length !== 0 ? '' : props.formFields.goodName} onChange={(e) => updateFormField(e, 'goodName')} />
-                <TextField size="small" id="filled-basic" label="Опис страви" multiline variant="filled" value={!props.formFields && props.formFields.description.length !== 0 ? '' : props.formFields.description} onChange={(e) => updateFormField(e, 'description')} />
-                <Select size="small"
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={props.formFields.category}
-                    onChange={(e) => updateFormField(e, 'category')}
-                    className={s.dialogSelect}
-                    style={{textTransform: 'capitalize'}}
-                >
-                    <MenuItem value={'Оберіть категорію'}>Оберіть категорію</MenuItem>
-                    {props.categories && props.categories.map((category, index) => (
-                        <MenuItem value={category} key={index} style={{textTransform: 'capitalize'}}>{category}</MenuItem>
-                    ))}
-                </Select>
-                <TextField type="number" size="small" id="filled-basic" label="Ціна ₴" variant="filled" value={!props.formFields.price ? '' : Number(props.formFields.price)} onChange={(e) => updateFormField(e, 'price')} />
-                <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    value={props.formFields.available}
-                    name="radio-buttons-group"
-                    onChange={(e) => updateFormField(e, 'available')}
-                >
-                    <FormControlLabel value='true' control={<Radio />} label="Доступно" />
-                    <FormControlLabel value='false' control={<Radio />} label="Недоступно" />
-                </RadioGroup>
-            </DialogContent>
-            <DialogActions className={s.dialogButtons}>
-                <Button variant="contained" onClick={closeDialog}>Закрити</Button>
-                {isSubmitting ?
-                    <Button color="secondary" variant="contained" disabled={true}>
-                        Відправка...
-                    </Button> :
-                    <Button color="secondary" variant="contained" onClick={handleSubmit} disabled={isDisabled()}>
-                        Відправити
-                    </Button>}
-            </DialogActions>
-        </Dialog>
+        <ThemeProvider theme={theme}>
+            <Dialog
+                classes={{ paper: s.dialog }}
+                onClose={closeDialog}
+                open={props.showDialog}
+                component="form">
+                <Typography variant="h4" className={s.dialogTitle}>
+                    {isEdit ? "РЕДАГУВАТИ" : "ДОДАТИ"} ТОВАР
+                </Typography>
+                <DialogContent className={s.dialogFields} >
+                    <Stack direction="row" spacing={2} className={s.dialogGoodImage}>
+                        {(isEdit) && <Avatar alt="receipt image" src={props.formFields.imgURL} />}
+                        <Button variant="outlined" component="label" color="secondary" className={s.dialogImgPicker}>
+                            Обрати зображення
+                            <input type="file" hidden onInput={(event) => { setFileData(event.target) }} accept=".jpg, .jpeg, .png, .jfif" />
+                        </Button>
+                        <Typography className={s.dialogPickedImg}>{props.formFields.fileName}</Typography>
+                    </Stack>
+                    <TextField size="small" id="filled-basic" label="Назва страви" variant="filled" value={!props.formFields && props.formFields.goodName.length !== 0 ? '' : props.formFields.goodName} onChange={(e) => updateFormField(e, 'goodName')} />
+                    <TextField size="small" id="filled-basic" label="Опис страви" multiline variant="filled" value={!props.formFields && props.formFields.description.length !== 0 ? '' : props.formFields.description} onChange={(e) => updateFormField(e, 'description')} />
+                    <Select size="small"
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={props.formFields.category}
+                        onChange={(e) => updateFormField(e, 'category')}
+                        className={s.dialogSelect}
+                        style={{ textTransform: 'capitalize' }}
+                    >
+                        <MenuItem value={'Оберіть категорію'}>Оберіть категорію</MenuItem>
+                        {props.categories && props.categories.map((category, index) => (
+                            <MenuItem value={category} key={index} style={{ textTransform: 'capitalize' }}>{category}</MenuItem>
+                        ))}
+                    </Select>
+                    <TextField type="number" size="small" id="filled-basic" label="Ціна ₴" variant="filled" value={!props.formFields.price ? '' : Number(props.formFields.price)} onChange={(e) => updateFormField(e, 'price')} />
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        value={props.formFields.available}
+                        name="radio-buttons-group"
+                        onChange={(e) => updateFormField(e, 'available')}
+                    >
+                        <FormControlLabel value='true' control={<Radio />} label="Доступно" />
+                        <FormControlLabel value='false' control={<Radio />} label="Недоступно" />
+                    </RadioGroup>
+                </DialogContent>
+                <DialogActions className={s.dialogButtons}>
+                    <Button variant="contained" onClick={closeDialog}>Закрити</Button>
+                    {!isSubmitting ?
+                        <Button color="secondary" variant="contained" disabled={true}>
+                            Відправка...
+                        </Button> :
+                        <Button color="secondary" variant="contained" onClick={handleSubmit} disabled={isDisabled()}>
+                            Відправити
+                        </Button>}
+                </DialogActions>
+            </Dialog>
+        </ThemeProvider>
     )
 }
 
